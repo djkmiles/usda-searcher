a7aae5ecb53740353276a4694e3502d22a8e47cb
djkmiles <github@danmiles.co.uk>
2014-08-18 21:52:05 +0100

    init

--- usda.js
+++ usda.js
@@ -13,14 +13,26 @@ if (Meteor.isClient)
 {
     Deps.autorun(function()
     {
-        Meteor.subscribe('food', Session.get('phrase'));
+        Meteor.subscribe('food', Session.get('phrase'), function()
+        {
+            $('li').tooltip({container: 'body'});
+        });
     });
-
     Template.search.data = function() { return Food.find() };
-
     Template.search.events({
         'change input': function(event) {
             Session.set('phrase', event.target.value);
         }
     });
+    Template.search.helpers({
+        isWanted: function(tagname) {
+            return tagname in info ? true : false;
+        },
+        nameOf: function(tagname) {
+            return tagname in info ? info[tagname] : '';
+        }
+    });
 }
+
+info = {'PROCNT': 'Protein', 'FAT': 'Fat', 'CHOCDF': 'Carb',
+    'ENERC_KCAL': 'Cal', 'SUGAR': 'Sugar', 'FIBTG': 'Fibre'};
